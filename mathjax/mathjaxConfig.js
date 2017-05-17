2fe407ee2c3fee5f05352049a10f8d9b9e7c1719
Joris Kinable <jkinable@gmail.com>
2017-05-17 17:50:29 -0400

    added mathjax configuration

---
+++ mathjax/mathjaxConfig.js
@@ -0,0 +1,7 @@
+MathJax.Hub.Config({
+  tex2jax: {
+    inlineMath: [['$','$'], ['\\(','\\)']],
+    processEscapes: true
+  }
+});
+MathJax.Ajax.loadComplete("http://jgrapht.org/mathjax/mathjaxConfig.js");
